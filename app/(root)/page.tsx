3a8809fd88c1005549a115cb02f540ea369c7a7e
mdarf <mdarfinji45@gmail.com>
2023-12-30 10:13:43 +0800

    added database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
+import Link from 'next/link'
 
 
 export default function Home() {
   return (
+  <>
     <section className="bg-primary-50 bg-dotted-pattern bg-containe py-5 md:py-10">
       
 
@@ -12,15 +14,52 @@ export default function Home() {
 
      <div  className='flex flex-col justify-center gap-8'>
 
-      <h1 className='h1-bold'>Host Connect Celebreate : Your Events, Our Platform </h1>
+      <h3 className='h3-bold'>Host Connect Celebreate : Your Events, Our Platform </h3>
+
+      <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+
+      {/* <Button size="lg"  asChild className="button w-full sm:w-fit">  
+         <Link href="#events">
+                Explore Now
+              </Link></Button> */}
+
+
+      <Button  asChild className="button  sm:w-fit">
+      <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+
+
+
 
       </div>
 
+      <Image
+         src='/assets/images/hero.png'
+          alt='hero'
+          width={1000}
+          height={1000}
+
+          className='max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]'
+         />
 
       </div>
 
 
     </section>
+
+
+   <section id='events'  className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+   <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          {/* <Search /> */}
+          {/* <CategoryFilter /> */}
+        </div>
+   </section>
+
+  </>
   )
 }
 
